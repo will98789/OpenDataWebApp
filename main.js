65eaca49a99f6579c5fa4e4bc31ad78fd7392a1f
Kip Parker <kipgparker@gmail.com>
2019-08-07 11:45:05 +0200

    add closing in main.js
    Added basic readme and added closing functionality to main.ks

--- main.js
+++ main.js
@@ -6,13 +6,28 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-
       nodeIntegration: true
     }
   })
-
   // and load the index.html of the app.
   win.loadFile('loading.html')
 }
 
 app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
