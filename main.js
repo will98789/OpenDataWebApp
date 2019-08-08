8fe6fd7e2d54e0acf6ef30f059a05f6b8f9374b0
Will <will947@hotmail.co.uk>
2019-08-08 16:24:32 +0200

    changed main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
 const { app, BrowserWindow } = electron;
+var currentWindow;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -11,16 +12,29 @@ async function createWindow () {
   let win = new BrowserWindow({
     width: width*0.8,
     height: height*0.8,
+    frame: false,
     webPreferences: {
       nodeIntegration: true
     }
   })
-  // and load the index.html of the app.
+  // and load the index.html of the app
+  currentWindow = win;
   win.loadFile('loading.html');
-  await sleep(2500);
-  win.loadFile('index.html');
-  let contents = win.webContents;
-  console.log(contents);
+  await sleep(500);
+  let iwin = new BrowserWindow({
+    width: 0,
+    height: height*0.8,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  iwin.hide();
+  iwin.loadFile('index.html');
+  iwin.setPosition(win.getPosition()[0],win.getPosition()[1]);
+  iwin.setSize(0,height*0.8);
+  iwin.show();
+  win.close()
 }
 
 app.on('ready', createWindow);
