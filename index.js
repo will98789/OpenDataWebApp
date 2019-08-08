dc5d8dc9f07f6999f5cff5b3c4403ac6d2297985
Will <will947@hotmail.co.uk>
2019-08-09 01:01:11 +0200

    added docs

--- index.js
+++ index.js
@@ -8,3 +8,12 @@ function openNav() {
 function closeNav() {
   document.getElementById("sideNav").style.width = "0";
 }
+
+function getDocs(){
+  var fs = require('fs');
+  var files = fs.readdirSync('docs');
+  var list = document.getElementById("docList");
+  for(var i = 0; i < files.length; i++){
+    list.innerHTML += "<li><a href='docs/"+files[i]+"/index.html'>"+files[i]+"</a></li>";
+  }
+}
