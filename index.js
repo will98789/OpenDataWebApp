cf75af1c54041449d4efacd5d7ea62fb1ccc5e23
Will <will947@hotmail.co.uk>
2019-08-12 12:34:25 +0200

    added hackathon docs

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function getDocs(){
   var files = fs.readdirSync('docs');
   var list = document.getElementById("docList");
   for(var i = 0; i < files.length; i++){
+    //Each folder needs to contain an info.json to interpret, with the file name, description and the entry file for that piece of documentation
     var json = JSON.parse(fs.readFileSync('docs/'+files[i]+'/info.json', 'utf8'));
     list.innerHTML += "<tr onclick=\"location.href='docs/"+files[i]+"/index.html'\" class='docListItem'><td><b>"+json.docName+"</b></td><td>"+json.description+"</td></tr>";
   }
